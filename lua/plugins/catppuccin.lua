11cbeff0a931fcfae47f5f672da05f060e5757ad
Mahaprasad Mohanty <mahaprasad003@gmail.com>
2025-12-01 21:34:55 +0530

    added catppuccin config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,56 @@
+-- ~/.config/nvim/lua/plugins/catppuccin.lua
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, 
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- The standard dark theme
+        transparent_background = false, -- Change to true if you want your wallpaper to show through
+        term_colors = true,
+        styles = {
+          comments = { "italic" }, -- Italics make code look 10x better
+          conditionals = { "italic" },
+        },
+        -- INTEGRATIONS (The Magic Part)
+        -- This ensures every plugin looks like part of a unified UI
+        integrations = {
+          cmp = true,          -- Styles the autocomplete menu
+          gitsigns = true,     -- Styles the git gutter
+          nvimtree = true,     
+          telescope = true,    -- Styles the search window
+          mason = true,        -- Styles the installer window
+          dashboard = true,    -- Styles your start screen
+          
+          -- Fixes the file explorer look
+          neotree = true,
+          
+          -- Styles the brackets
+          rainbow_delimiters = true,
+          
+          -- Styles the indentation lines
+          indent_blankline = {
+            enabled = true,
+            scope_color = "lavender", 
+            colored_indent_levels = false,
+          },
+          
+          -- Styles the LSP errors (underlines vs squiggles)
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
+        },
+      })
+
+      -- Actually load the theme
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
- 
